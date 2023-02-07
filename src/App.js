fea1049f096ab6f09cfe3d064aa0ae784ae8f7bc
Majbahia <majbahia@gmail.com>
2023-02-07 13:15:19 +0600

    Showing country name and population in the display

--- src/App.js
+++ src/App.js
@@ -20,8 +20,18 @@ function LoadCountries(){
     <div>
       <h1>Visiting  Every Countr in the world!</h1>
       <h3>Available Countries: {counstries.length}</h3>
+      {
+        counstries.map(country=> <Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   )
 }
-
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+}
 export default App;
